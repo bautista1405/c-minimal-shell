754ffd4a1550dc4345284eaa7752cacbe2317e9b
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2024-05-28 20:11:49 -0300

    managing user input with memory alloc & realloc

--- src/main.c
+++ src/main.c
@@ -26,3 +26,48 @@ void lsh_loop(void)
 
     } while (status);
 }
+
+#define LSH_RL_BUFSIZE 1024
+char *lsh_read_line(void)
+{
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
+
+    if (!buffer)
+    {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1)
+    {
+        // read a character
+        c = getchar();
+
+        // if we hit EOF replace it with a null character and return
+        if (c == EOF || c == '\n')
+        {
+            buffer[position] = '\0';
+            return buffer;
+        }
+        else
+        {
+            buffer[position] = c;
+        }
+        position++;
+
+        // if exceeding the buffer we reallocate
+        if (position >= bufsize)
+        {
+            bufsize += LSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer)
+            {
+                frintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
