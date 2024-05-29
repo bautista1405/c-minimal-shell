ff924ddfb79e479aa773ec70c6b18bad4bd8e716
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2024-05-29 17:51:21 -0300

    changed code/fns order and fixed some typos

--- src/main.c
+++ src/main.c
@@ -1,32 +1,122 @@
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+// function declarations for built-in shell commands
+// forward declaration is when you declare (but don’t define) something, so that you can use its name before you define it
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
 
-int main(int argc, char **argv)
+// list of built-in commands followed by their corresponding functions
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"};
+
+// built-in commands can be added simply by modifying these arrays
+int (*builtin_func[])(char **) = {
+    &lsh_cd,
+    &lsh_help,
+    &lsh_exit};
+
+int lsh_num_builtins()
 {
-    // call our loop fn to be executed while terminal is being used
-    lsh_loop();
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+// built-in functions implementation
+int lsh_cd(char **args)
+{
+    if (args[1] == NULL)
+    {
+        fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+    }
+    else
+    {
+        if (chdir(args[1]) != 0)
+        {
+            perror("lsh");
+        }
+    }
+    return 1;
+}
+
+int lsh_help(char **args)
+{
+    int i;
+    printf("Stephen Brennan's LSH\n");
+    printf("Type program names and arguments, and hit enter.\n");
+    printf("The following are built in:\n");
+
+    for (i = 0; i < lsh_num_builtins(); i++)
+    {
+        printf(" %s\n", builtin_str[i]);
+    };
+
+    printf("Use the man command for information on other programs.\n");
+    return 1;
+}
+
+int lsh_exit(char **args)
+{
     return 0;
 }
 
-void lsh_loop(void)
+int lsh_launch(char **args)
 {
-    char *line;
-    char **args;
+    //`pid_t` for uniquely identifying and managing processes in Unix-like systems
+    pid_t pid, wpid;
     int status;
 
-    // here the loop fns are pretty self-explanatory
-    do
+    // create new processes (parent and child)
+    pid = fork();
+    if (pid == 0)
     {
+        // child process
+        if (execvp(args[0], args) == -1)
+        {
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    }
+    else if (pid < 0)
+    {
+        // error forking
+        perror("lsh");
+    }
+    else
+    {
+        // parent process
+        do
+        {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return 1;
+}
 
-        printf("> ");                // print command
-        line = lsh_read_line();      // read command
-        args = lsh_split_line(line); // splint command into args
-        status = lsh_execute(args);  // execute args
+int lsh_execute(char **args)
+{
+    int i;
 
-        // free memory
-        free(line);
-        free(args);
+    if (args[0] == NULL)
+    {
+        // an empty command was entered
+        return 1;
+    }
 
-    } while (status);
+    for (i = 0; i < lsh_num_builtins(); i++)
+    {
+        if (strcmp(args[0], builtin_str[i]) == 0)
+        {
+            return (*builtin_func[i])(args);
+        }
+    }
+    return lsh_launch(args);
 }
 
 #define LSH_RL_BUFSIZE 1024
@@ -98,7 +188,7 @@ char **lsh_split_line(char *line)
         if (position >= bufsize)
         {
             bufsize += LSH_TOK_BUFSIZE;
-            tokens += realloc(tokens, bufsize * sizeof(char *));
+            tokens = realloc(tokens, bufsize * sizeof(char *));
             if (!tokens)
             {
                 fprintf(stderr, "lsh: allocation error\n");
@@ -111,116 +201,31 @@ char **lsh_split_line(char *line)
     return tokens;
 }
 
-int lsh_launch(char **args)
+void lsh_loop(void)
 {
-    //`pid_t` for uniquely identifying and managing processes in Unix-like systems
-    pid_t pid, wpid;
+    char *line;
+    char **args;
     int status;
 
-    // create new processes (parent and child)
-    pid = fork();
-    if (pid == 0)
+    // here the loop fns are pretty self-explanatory
+    do
     {
-        // child process
-        if (execvp(args[0], args) == -1)
-        {
-            perror("lsh");
-        }
-        exit(EXIT_FAILURE);
-    }
-    else if (pid < 0)
-    {
-        // error forking
-        perror("lsh");
-    }
-    else
-    {
-        // parent process
-        do
-        {
-            wpid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
-    return 1;
+
+        printf("> ");                // print command
+        line = lsh_read_line();      // read command
+        args = lsh_split_line(line); // splint command into args
+        status = lsh_execute(args);  // execute args
+
+        // free memory
+        free(line);
+        free(args);
+
+    } while (status);
 }
 
-// function declarations for built-in shell commands
-// forward declaration is when you declare (but don’t define) something, so that you can use its name before you define it
-int lsh_cd(char **args);
-int lsh_help(char **args);
-int lsh_exit(char **args);
-
-// list of built-in commands followed by their corresponding functions
-char *builtin_str[] = {
-    "cd",
-    "help",
-    "exit"};
-
-// built-in commands can be added simply by modifying these arrays
-int (*builtin_func[])(char **) = {
-    &lsh_cd,
-    &lsh_help,
-    &lsh_exit};
-
-int lsh_num_builtins()
+int main(int argc, char **argv)
 {
-    return sizeof(builtin_str) / sizeof(char *);
-}
-
-// built-in functions implementation
-int lsh_cd(char **args)
-{
-    if (args[1] == NULL)
-    {
-        fprintf(stderr, "lsh: expected argument to \"cd\"\n");
-    }
-    else
-    {
-        if (chdir(args[1]) != 0)
-        {
-            perror("lsh");
-        }
-    }
-    return 1;
-}
-
-int lsh_help(char **args)
-{
-    int i;
-    printf("Stephen Brennan's LSH\n");
-    printf("Type program names and arguments, and hit enter.\n");
-    printf("The following are built in:\n");
-
-    for (i = 0; i < lsh_num_builtins(); i++)
-    {
-        printf(" %s\n", builtin_str[i]);
-    };
-
-    printf("Use the man command for information on other programs.\n");
-    return 1;
-}
-
-int lsh_exit(char **args)
-{
+    // call our loop fn to be executed while terminal is being used
+    lsh_loop();
     return 0;
-}
-
-int lsh_execute(char **args)
-{
-    int i;
-
-    if (args[0] == NULL)
-    {
-        // an empty command was entered
-        return 1;
-    }
-
-    for (i = 0; i < ls_num_builtins(); i++)
-    {
-        if (strcmp(args[0], builtin_str[i] == 0))
-        {
-            return (*builtin_func[i])(args);
-        }
-    }
-    return lsh_launch(args);
 }
